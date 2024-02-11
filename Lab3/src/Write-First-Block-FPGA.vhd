280ff129841ab0245773c6b7d76d110e9a4ca76d
Tacitor <lukasjkrampitz@gmail.com>
2024-02-10 20:37:07 -0500

    finish vdh and xdc needed to run RAM on hardware

--- Lab3/src/Write-First-Block-FPGA.vhd
+++ Lab3/src/Write-First-Block-FPGA.vhd
@@ -16,30 +16,30 @@ architecture Behavioural of lab2mem is
 type memType is array(0 to 2**Awidth-1) of std_logic_vector(Dwidth-1 downto 0);
 
 -- The first 8 locations are initialized, the rest set to 0.
-signal memory: memType:= ( "0000000000000001",
-                           "0000000000000010",
-                           "0000000000000100",
-                           "0000000000001000",
-                           "0000000000010000",
-                           "0000000000100000",
-                           "0000000001000000",
-                           "0000000010000000",
-                        others=> "0000000000000000" ); 
+signal memory: memType:= ( "0001", --0
+                           "0010", --1
+                           "0100", --2
+                           "1000", --3
+                           "0111", --4
+                           "1011", --5
+                           "1101", --6
+                           "1110", --7
+                        others=> "1111" ); 
 
 attribute ram_style: string;
 attribute ram_style of memory : signal is "block"; --Valid attributes include block, distributed and auto
-                        
+
 begin
     process(clk)
     begin
-        if(clk'event and clk='1') then            
+        if(clk'event and clk='1') then
             if(we='1') then
-                memory(conv_integer(addr)) <= din; 
-                dout <= din; 
+                memory(conv_integer(addr)) <= din;
+                dout <= din;
             else
-                dout <= memory(conv_integer(addr)); 
-            end if; 
-        end if; 
+                dout <= memory(conv_integer(addr));
+            end if;
+        end if;
         
     end process;
 end Behavioural;      
